8de828dfdb37113d0a7be9dd191b3292604a5738
Sean Shookman <sms112788@gmail.com>
2019-06-14 15:28:39 -0500

    Adds snippet highlighting based on backticks

--- mindlogger/mindbase.py
+++ mindlogger/mindbase.py
@@ -29,33 +29,55 @@ class mindbase():
         self.conn.commit()
 
     def show(self, query="SELECT * FROM mindlog ORDER BY date DESC LIMIT 100", activeTask="None"):
+        rows = []
+        for row in self.cursor.execute(query):
+            rows.append(row)
+
+        self.displayLog(rows, "None", "", activeTask)
+
+    def displayLog(self, rows, task, date, activeTask):
         #TODO: This should be configurable by the user and placed in a config somewhere
         activeTaskColor="\033[0m\033[36m\033[1m"
         dateColor="\033[0m\033[44m\033[1m"
         endColor="\033[0m"
 
+        # Clear screen, and display logs row by row (oldest to newest)
         print('\x1bc')
-        task = "None"
-        date = ""
-        rows = []
-        for row in self.cursor.execute(query):
-            rows.append(row)
         for row in reversed(rows):
-            latestDate = datetime.strptime(row[0], "[%Y/%m/%d %H:%M:%S]").date()
+            # Obtain row values
+            date_time = row[0]
+            task_name = row[1]
+            message  = row[2]
+
+            # Print the date if it has changed
+            latestDate = datetime.strptime(date_time, "[%Y/%m/%d %H:%M:%S]").date()
             if (date != latestDate):
                 date = latestDate
                 task = "None"
                 print("\n{col}                              {date}                              {end}".format(date=date, col=dateColor, end=endColor))
-            if (task != row[1]):
-                task = row[1]
+
+            # Update the task, color, and display the name if it has changed
+            if (task != task_name):
+                task = task_name
                 if (task == "None"):
                     print("\n")
                 else:
                     col = activeTaskColor if activeTask == task else endColor
                     print("\n{col}_____{task}_____".format(task=task, col=col))
-                
-            print(row[0], " | ", row[2])
+
+            # Print the log
+            print(date_time, "|", self.formatMessage(message, col))
+
         print(endColor)
+
+    def formatMessage(self, message, col=""):
+        # Replace sections of `code` with proper highlighting
+        snippet_char = "`"
+        while (message.count(snippet_char) >= 2):
+            message = message.replace(snippet_char, "\033[0m\033[107m\033[30m ", 1)
+            message = message.replace(snippet_char, " \033[0m{col}".format(col=col), 1)
+
+        return message
 
     def search(self, context):
         query = "SELECT * FROM mindlog{where} ORDER BY date DESC"
